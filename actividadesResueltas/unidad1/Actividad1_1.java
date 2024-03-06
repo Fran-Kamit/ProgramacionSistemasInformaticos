27049d7f080089d3c8f89a238f7eb1c0cc78c77d
Francisco José <65421353+Fran-Kamit@users.noreply.github.com>
2024-03-06 21:10:22 +0100

    Delete actividadesResueltas/unidad1 directory

--- actividadesResueltas/unidad1/Actividad1_1.java
+++
@@ -1,8 +0,0 @@
-package unidad1;
-
-public class Actividad1_1 {
-    public static void main(String[] args) {
-        System.out.println("Escribir unas líneas de código que saluden al usuario con el mensaje: «Hola. Encantado de conocerlo.». \n");
-        System.out.print("Hola. Encantado de conocerlo.");
-    }
-}
