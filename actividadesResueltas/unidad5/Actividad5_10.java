85869ced8334a6b4ff5ce237fffb27bdbf22563e
Francisco José <65421353+Fran-Kamit@users.noreply.github.com>
2024-03-06 21:04:43 +0100

    Add files via upload

---
+++ actividadesResueltas/unidad5/Actividad5_10.java
@@ -0,0 +1,42 @@
+package unidad5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Actividad5_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, datos[], numero;
+
+        System.out.print("Escriba n: ");
+        n = sc.nextInt();
+        datos = new int[n]; //crear tabla de longitud n
+
+        for (int i = 0; i < datos.length; i++){ //asignamos valores a la tabla
+            System.out.print("Introduzca un número: ");
+            datos[i] = sc.nextInt();
+        }
+
+        System.out.println("Valores de la tabla: " + Arrays.toString(datos));
+
+        System.out.println("\nIntroduzca a partir de que nº desea eliminar: ");
+        numero = sc.nextInt();
+
+        int sinMayores[] = sinMayores(datos, numero);
+        System.out.println("Valores de la tabla sin nº mayores a " + numero + " -> " + Arrays.toString(sinMayores));
+
+    }
+    static int [] sinMayores(int t[], int valor){
+        int copia[] = Arrays.copyOf(t, t.length);
+        int i = 0;
+        while (i < copia.length){
+            if (copia[i] > valor){
+                copia[i] = copia[copia.length - 1];
+                copia = Arrays.copyOf(copia, copia.length - 1);
+            } else {
+                i++;
+            }
+        }
+        return copia;
+    }
+}
